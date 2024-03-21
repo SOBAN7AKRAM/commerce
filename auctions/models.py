40f3f1c522654cf684b521b5f39b47bd0274374c
M Soban Akram <chsobanakram@gmail.com>
2024-03-21 22:00:34 +0500

    After adding models, navbar, createlisting Ui

--- auctions/models.py
+++ auctions/models.py
@@ -1,6 +1,39 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext as _
 
 
 class User(AbstractUser):
     pass
+class AuctionListing(models.Model):
+    class categories(models.TextChoices):
+        fashion = 'Fsh', _('Fashion')
+        toys = 'Ty', _('Toys')
+        electronics = 'Elec', _('Electronics')
+        books = 'Book', _('Books')
+        sports = 'sports', _('Sports')
+        none = '', _('None')
+    title = models.CharField(max_length=64)
+    description = models.TextField()
+    image = models.ImageField(blank=True)
+    initialBid = models.IntegerField(default = 0)
+    category = models.CharField(max_length=64, choices = categories, default = categories.none, blank = True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name = "owner")
+    
+    def __str__(self):
+        return f"{self.title}"
+
+class Bid(models.Model):
+    bidOn = models.ForeignKey(AuctionListing, on_delete = models.CASCADE, related_name = "bidOn")
+    bid = models.IntegerField()
+    buyer = models.ForeignKey(User, on_delete=models.CASCADE, related_name = "buyer")
+    def __str__(self):
+        return f"{self.buyer.first_name} made {self.bid} on {self.bidOn.title}"
+    
+    
+class Comments(models.Model):
+    commentedOn = models.ForeignKey(AuctionListing, on_delete = models.CASCADE, related_name = "commentsOn")
+    Comment = models.TextField()
+    commentor = models.ForeignKey(User, on_delete = models.CASCADE, related_name = "commentor")
+    def __str__(self):
+        return f"{self.commentor} comment '{self.Comment}' on {self.commentedOn.title}" 
--- auctions/views.py
+++ auctions/views.py
@@ -1,8 +1,12 @@
 from django.contrib.auth import authenticate, login, logout
+from . models import AuctionListing, Bid, Comments
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django import forms
+from django.utils.safestring import mark_safe
+from django.core.validators import MinValueValidator
 
 from .models import User
 
@@ -35,6 +39,41 @@ def logout_view(request):
     logout(request)
     return HttpResponseRedirect(reverse("index"))
 
+class createListingForm(forms.Form):
+    title = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title'}),
+        required=True,
+        label=mark_safe('Title <span class="required">*</>')
+    )
+    description = forms.CharField(
+        widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Description'}),
+        required=True,
+        label=mark_safe('Description <span class="required">*</>')
+    )
+    initialBid = forms.IntegerField(
+        required=True,
+        validators=[MinValueValidator(1)],
+        label=mark_safe('Initial Bid <span class="required">*</>')
+    )
+    picture = forms.ImageField(
+        label = 'Choose an Image:',
+        required=False
+    )
+    category = forms.ChoiceField(
+        label = 'Category:',
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        choices=AuctionListing.categories.choices,
+        required=False
+    )
+    initialBid.widget.attrs.update({'class': 'form-control', 'placeholder': "Initial Bid"})
+    picture.widget.attrs.update({'class': 'form-control'})
+    
+    
+def createListing(request):
+    form = createListingForm(label_suffix='')
+    return render(request, "auctions/create.html", {
+        'form': form,
+    })
 
 def register(request):
     if request.method == "POST":
